f2590d4489ef7f0025d1a5cf236d572b6990bbaf
Raí Joia <megac154@gmail.com>
2024-08-29 23:22:05 -0300

    feat: uuid in db

---
+++ prisma/migrations/20240830022122_add/migration.sql
@@ -0,0 +1,25 @@
+/*
+  Warnings:
+
+  - The primary key for the `User` table will be changed. If it partially fails, the table could be left without primary key constraint.
+
+*/
+-- RedefineTables
+PRAGMA defer_foreign_keys=ON;
+PRAGMA foreign_keys=OFF;
+CREATE TABLE "new_User" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "email" TEXT NOT NULL,
+    "username" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "ativo" BOOLEAN NOT NULL DEFAULT true,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL,
+    "deletedAt" DATETIME
+);
+INSERT INTO "new_User" ("ativo", "createdAt", "deletedAt", "email", "id", "password", "updatedAt", "username") SELECT "ativo", "createdAt", "deletedAt", "email", "id", "password", "updatedAt", "username" FROM "User";
+DROP TABLE "User";
+ALTER TABLE "new_User" RENAME TO "User";
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+PRAGMA foreign_keys=ON;
+PRAGMA defer_foreign_keys=OFF;
