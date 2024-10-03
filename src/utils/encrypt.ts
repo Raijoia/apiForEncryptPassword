a26bdff2eb4a29747b684cdf17f7bee8b6710fac
Raí Joia <megac154@gmail.com>
2024-10-03 20:17:25 -0300

    fix: change encrypt for matrix and matrix transposta

--- src/utils/encrypt.ts
+++ src/utils/encrypt.ts
@@ -1,21 +1,44 @@
-import { User } from '@prisma/client';
-
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const bcrypt = require('bcrypt');
+import type { User } from '@prisma/client';
 
 export function encryptPassword(password: string): Promise<string> {
-  const saltRounds = process.env.SALT_ROUNDS;
+  const passwordString = password.toString();
 
-  return bcrypt.hash(password, parseInt(saltRounds));
+  const cleanText = passwordString.replace(/[^a-zA-Z0-9]/g, '');
+
+  const rows = Math.ceil(cleanText.length / 4);
+
+  const matrix: string[][] = [];
+
+  let currentIndex = 0;
+  for (let r = 0; r < rows; r++) {
+    matrix[r] = [];
+    for (let c = 0; c < 4; c++) {
+      matrix[r][c] = cleanText[currentIndex] || ' ';
+      currentIndex++;
+    }
+  }
+
+  let encryptedText = '';
+  for (let c = 0; c < 4; c++) {
+    for (let r = 0; r < rows; r++) {
+      encryptedText += matrix[r][c];
+    }
+  }
+
+  console.log('senha ' + encryptedText);
+
+  return Promise.resolve(encryptedText);
 }
 
 export async function comparetor(
   password: string,
   hash: string,
 ): Promise<User | boolean> {
-  if (await bcrypt.compare(password, hash)) {
-    return true;
-  } else {
+  const passwordEncrypted = await encryptPassword(password);
+
+  if (passwordEncrypted !== hash) {
     return false;
   }
+
+  return true;
 }
